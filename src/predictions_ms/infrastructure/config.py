6aaf4209c397fae65468d6dc3f880ead4b9d50b4
Mariano Carreira <mariano.carreira@cscgfm.com>
2024-01-07 17:28:59 -0300

    v 1.9

--- src/predictions_ms/infrastructure/config.py
+++ src/predictions_ms/infrastructure/config.py
@@ -1,5 +1,12 @@
-import json
+import json, os
 
-with open('./config.json') as config_file:
+flask_env = os.environ.get('FLASK_ENV', 'development')
+
+if flask_env == "development":
+    configPath = "./config.json"
+else:
+    configPath = "./config_docker.json"
+
+with open(configPath) as config_file:
     config_data = json.load(config_file)
     print(config_data)
--- src/users_ms/api/controllers.py
+++ src/users_ms/api/controllers.py
@@ -3,7 +3,9 @@ from .  import users_internal_api_blueprint
 from domain import users_service as svc
 from infrastructure.config import config_data
 
-@users_internal_api_blueprint.route(f'{config_data["API_BASE_URL"]}/user/auth', methods=['POST'])
+USERS_AUTH_ENDPOINT_URL = (config_data["API_BASE_URL"]+"/user/auth")
+print(USERS_AUTH_ENDPOINT_URL)
+@users_internal_api_blueprint.route(USERS_AUTH_ENDPOINT_URL, methods=['POST'])
 def auth_post():
     user = svc.authUser(request.headers.get(config_data["API_KEY_HEADER"]))
     if not user:
